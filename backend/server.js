4b3c3212993300700fa6452e33fc71526406e4b1
soyal-chawhan <soyalchawhan@gmail.com>
2026-04-23 20:31:18 +0530

    updating frontend url

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const allowedOrigins = [
   'http://localhost:5500',
   'http://127.0.0.1:3000',
   'http://localhost:3000',
-  'bugtrack.vercel.app',
+  'bugtrack-soyalchawhans-projects.vercel.app',
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
